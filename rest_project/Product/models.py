2ef0dd5ddd9713e0d863b98bd315e4f6ad59a77a
bzhumakova <bzhumakova@gmail.com>
2021-06-15 18:22:16 +0600

    create Category, Product models

---
+++ rest_project/Product/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Category(models.Model):
+    title=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+class Product(models.Model):
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    photo=models.ImageField(blank=True,null=True)
+    name=models.CharField(max_length=50)
+    price=models.FloatField(default=0.0)
+    desc=models.TextField()
+
+    def __str__(self):
+        return self.name
