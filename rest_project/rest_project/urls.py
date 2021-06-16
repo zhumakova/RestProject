4e093cbd821bb75220f6a959d40fc284b8c1a8b7
bzhumakova <bzhumakova@gmail.com>
2021-06-16 18:24:43 +0600

    register path for comments app

--- rest_project/rest_project/urls.py
+++ rest_project/rest_project/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('userprofile.urls')),
-    path('',include('Product.urls'))
+    path('',include('Product.urls')),
+    path('',include('comments.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
