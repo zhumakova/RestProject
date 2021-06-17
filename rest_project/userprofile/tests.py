fff1b2008762053aff7a4bd8e7a0e977228c70af
bzhumakova <bzhumakova@gmail.com>
2021-06-17 18:10:52 +0600

    test for Register

--- rest_project/userprofile/tests.py
+++ rest_project/userprofile/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import *
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+class RegisterTest(APITestCase):
+
+    def setUp(self)->None:
+        self.url=reverse('register')
+
+    def test_register_ok(self):
+        date={
+
+                "username": "maksimka1234",
+                "password": "123456",
+                "check_password": "123456",
+                "profile": {
+                    "full_name": "maksimka",
+                    "address":"troitskaya",
+                    "wallet":50000,
+                    "email":"a@j"
+                }
+
+        }
+        self.response=self.client.post(self.url,date,format='json')
+        self.assertEqual(self.response.status_code,201)
