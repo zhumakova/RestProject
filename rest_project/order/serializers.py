6fab8a3c59f267c8f93d5fadf7c007fedcb8258d
bzhumakova <bzhumakova@gmail.com>
2021-06-17 18:15:30 +0600

    serializer for Order model

---
+++ rest_project/order/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+    total_sum=serializers.SerializerMethodField()
+
+    class Meta:
+        model=Order
+        fields='__all__'
+
+    def get_total_sum(self,obj):
+        total_sum=obj.product.price*obj.quantity
+        obj.total_sum=total_sum
+        obj.save()
+        return total_sum
+
