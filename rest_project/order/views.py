545b55b11174e1ccdcbd598c1c636d6a5d1e6338
bzhumakova <bzhumakova@gmail.com>
2021-06-21 18:19:06 +0600

    create UpdateDeleteOrder View to update order

--- rest_project/order/views.py
+++ rest_project/order/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import *
@@ -14,3 +15,26 @@ class OrderPostView(APIView):
             serializer.save()
             return Response(serializer.data,status=201)
         return Response(serializer.errors,status=400)
+
+class MyOrderView(APIView):
+    def get(self, request):
+        profile = request.user.profile
+        orders = profile.orders.all()
+        serializer = MyOrderSerializer(orders, many=True)
+        return Response(serializer.data)
+
+class UpdateDeleteOrder(APIView):
+    def put(self,request,*args,**kwargs):
+        order=Order.objects.get(id=kwargs['order_id'])
+        order_date=order.date_created
+        order_status=order.status
+        date_now=timezone.now()
+        delta=timezone.timedelta(minutes=10)
+        serializer=OrderSerializer(order,data=request.data)
+        if (date_now-order_date)<=delta and order_status=='in_process':
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors)
+        return Response('Time is up!')
+
