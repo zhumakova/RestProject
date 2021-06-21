ba06abcad58041be9d42f2583a13728cb391d506
bzhumakova <bzhumakova@gmail.com>
2021-06-21 18:12:55 +0600

    add path for login

--- rest_project/userprofile/urls.py
+++ rest_project/userprofile/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns=[
     path('profile/',ProfileView.as_view()),
     path('register/',RegisterView.as_view(),name='register'),
+    path('login/',LoginView.as_view(),name='login'),
 ]
