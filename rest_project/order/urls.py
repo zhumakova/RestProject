d81f28cb8fbc8e0df9ab78b3a4161e3decb1dbc9
bzhumakova <bzhumakova@gmail.com>
2021-06-21 18:14:59 +0600

    add paths for my_orders and modify_order pages

--- rest_project/order/urls.py
+++ rest_project/order/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns=[
-    path('order/',OrderPostView.as_view())
+    path('order/',OrderPostView.as_view()),
+    path('my_orders',MyOrderView.as_view()),
+    path('modify_order/<int:order_id>/',UpdateDeleteOrder.as_view())
 ]
